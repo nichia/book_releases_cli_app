367677f01280e1d4d26e8832d00176710d6a1f40
Ni Chia <nichia@gmail.com>
2018-02-05 18:09:30 +0000

    update newbooks

--- lib/book_releases_cli_app/newbooks.rb
+++ lib/book_releases_cli_app/newbooks.rb
@@ -1,12 +1,43 @@
 class BookReleasesCliApp::NewBooks
+  attr_accessor :title, :author, :release_date, :price
 
   def self.all
+    @@all = []
+
     puts <<-DOC.gsub /^\s*/, ''
       1. The Great Alone  by Kristin Hannah  Available: February 6 2018
       2. Girl, Wash Your Face  by Rachel Hollis  Available: February 6 2018
       3. Look for Me  by Lisa Gardner  Available: February 6 2018
     DOC
 
+    newbook_1 = self.new
+    newbook_1.title = "The Great Alone"
+    newbook_1.author = "Kristin Hannah"
+    newbook_1.release_date = "February 6 2018"
+    newbook_1.price = "$10"
+    puts newbook_1, newbook_1.title
+    @@all << newbook_1
+
+    newbook_2 = self.new
+    newbook_2.title = "Girl, Wash Your Face"
+    newbook_2.author = "Rachel Hollis"
+    newbook_2.release_date = "February 6 2018"
+    newbook_2.price = "$15"
+    @@all << newbook_2
+    puts newbook_2
+
+    newbook_3 = self.new
+    newbook_3.title = "Look for Me"
+    newbook_3.author = "Lisa Gardner"
+    newbook_3.release_date = "February 6 2018"
+    newbook_3.price = "$20"
+    @@all << newbook_3
+    puts newbook_3
+
+    puts @@all
+    @@all
+
+    #[newbook_1, newbbook_2, newbook_3]
   end
 
 end
