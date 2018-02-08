23d3fa7531d9c89950ba1942f5e1bf5b4a8f7c93
Ni Chia <nichia@gmail.com>
2018-02-08 22:26:57 +0000

    Updated scraper

--- lib/book_releases_cli_app/cli.rb
+++ lib/book_releases_cli_app/cli.rb
@@ -1,14 +1,20 @@
 class BookReleasesCliApp::CLI
 
   def call
+    load_new_releases
     list_books
     menu
+  end
+
+  def load_new_releases
+    puts "Loading new releases from the Book store website..."
+
+    @newbooks = BookReleasesCliApp::NewBooks.all
   end
 
   def list_books
     puts "New Book Releases"
 
-    @newbooks = BookReleasesCliApp::NewBooks.all
     @newbooks.each.with_index(1) do |newbook, i|
       puts "#{i}. #{newbook.title} - #{newbook.author} - #{newbook.release_date}"
     end
@@ -20,10 +26,11 @@ class BookReleasesCliApp::CLI
       puts "Enter the book number for more information or type list to see the books again or type exit"
       input = gets.strip.downcase
 
-      if input.to_i > 0 && input.to_i <= (@newbooks.count - 1)
+      if input.to_i > 0 && input.to_i <= @newbooks.count
         newbook = @newbooks[input.to_i - 1]
-        puts "Title: #{newbook.title}  Author: #{newbook.author}  Release Date: #{newbook.release_date}  Format: #{type}  Price: #{price}"
-        puts "OVERVIEW: #{overview}"
+        puts "Title: #{newbook.title}  Author: #{newbook.author}  Release Date: #{newbook.release_date}"
+        puts "Format: #{newbook.type}  Price: #{newbook.price}"
+        puts "OVERVIEW: #{newbook.overview}"
       elsif input == "list"
         list_books
       elsif input == "exit"
