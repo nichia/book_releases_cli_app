d2ab7842244d7984f601f2372d59335e607a9c79
Ni Chia <nichia@gmail.com>
2018-02-12 21:52:24 +0000

    Updated CLI menu

--- lib/book_releases_cli_app/cli.rb
+++ lib/book_releases_cli_app/cli.rb
@@ -2,20 +2,48 @@ class BookReleasesCliApp::CLI
 
   def call
     load_new_releases
-    list_books
+    list_books_menu
     menu
   end #-- call --
 
   def load_new_releases
-    #puts "Loading new releases from the Books a Million website..."
-    #@newbooks = BookReleasesCliApp::NewBooks.bam_scraper
+    puts "Loading new releases from Books a Million website..."
+    @newbooks_bam = BookReleasesCliApp::NewBooks.bam_scraper
 
-    puts "Loading new releases from the Barnes and Noble website..."
-    @newbooks = BookReleasesCliApp::NewBooks.bn_scraper
+    puts "Loading new releases from Barnes and Noble website..."
+    @newbooks_bn = BookReleasesCliApp::NewBooks.bn_scraper
   end #-- load_new_releases
 
-  def list_books
-    puts "New Book Releases"
+  def list_books_menu
+    input = nil
+    while input.to_i != 3
+      puts "New Book Releases"
+      puts "================="
+      puts "[1] Books a Million New Releases"
+      puts "[2] Barnes & Noble New Releases"
+      puts "[3] Exit"
+      puts "Enter your choice: "
+      input = gets.strip.downcase
+
+      case input.to_i
+      when 1
+        @newbooks = @newbooks_bam
+        list_books("Books a Million")
+      when 2
+        @newbooks = @newbooks_bn
+        list_books("Barnes & Noble")
+      when 3
+        puts "Thank you for using New Book Releases"
+      when other
+        puts "Incorrect enty, type list or exit"
+      end
+    end
+  end #-- list_books_menu
+
+  def list_books(name)
+    puts " "
+    puts "#{name} New Releases"
+    puts " "
 
     @newbooks.each.with_index(1) do |newbook, i|
       puts "#{i}. #{newbook.title} - #{newbook.author} - #{newbook.release_date}"
